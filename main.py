a49bc7976c929747f9a5755ec0259f735b062cc0
Sherwin Kartick <rskartick@gmail.com>
2023-08-02 18:20:32 -0400

    Parsing stops and vehicle locations

--- main.py
+++ main.py
@@ -13,16 +13,30 @@ class Stop:
 
 
 class Direction:
-    def __init__(self, tag, title, name, use_for_ui, branch):
+    def __init__(self, tag, title, name, branch):
         self.tag = tag
         self.title = title
         self.name = name
-        self.use_for_ui = use_for_ui
         self.branch = branch
         self.stops = []
 
     def add_stop(self, stop):
         self.stops.append(stop)
+
+
+class VehicleLocationSnapshot:
+    def __init__(self, vehicle_id, route_tag, dir_tag, lat, lon, secs_since_report, predictable, heading, speed_km_hr,
+                 last_time):
+        self.vehicle_id = vehicle_id
+        self.route_tag = route_tag
+        self.dir_tag = dir_tag
+        self.lat = lat
+        self.lon = lon
+        self.secs_since_report = secs_since_report
+        self.predictable = predictable
+        self.heading = heading
+        self.speed_km_hr = speed_km_hr
+        self.last_time = last_time
 
 
 def print_hi(name):
@@ -45,10 +59,10 @@ def main():
     if __name__ == '__main__':
         print_hi('PyCharm')
 
+    stops = []
+
     root = eT.fromstring(routeconfig_module.routeConfig)
     stop_elements = root.findall("./route/stop")
-
-    stops = []
 
     for stop_element in stop_elements:
         stop_tag = stop_element.get("tag")
@@ -61,7 +75,44 @@ def main():
         stop_obj = Stop(stop_tag, stop_title, stop_lat, stop_lon, stop_stop_id)
         stops.append(stop_obj)
 
-    print_stops(stops)
+    directions = []
+    direction_elements = root.findall("./route/direction")
+
+    for direction_element in direction_elements:
+        direction_obj = Direction(
+            tag=direction_element.get('tag'),
+            title=direction_element.get('title'),
+            name=direction_element.get('name'),
+            branch=direction_element.get('branch')
+        )
+        direction_stops_elements = direction_element.findall("./stop")
+        for direction_stops_element in direction_stops_elements:
+            direction_stop_obj = [stop for stop in stops if stop.tag == direction_stops_element.get("tag")][0]
+            direction_obj.add_stop(direction_stop_obj)
+        directions.append(direction_obj)
+
+    root = eT.fromstring(vehiclelocations_module.vehicleLocations)
+    last_time = root.find("./lastTime").get("time")
+
+    vehicle_location_snapshots = []
+    vehicle_elements = root.findall("./vehicle")
+    for vehicle_element in vehicle_elements:
+        vehicle_location_snapshot_obj = VehicleLocationSnapshot(
+            vehicle_id=vehicle_element.get("vehicle_id"),
+            route_tag=vehicle_element.get("route_tag"),
+            dir_tag=vehicle_element.get("dir_tag"),
+            lat=vehicle_element.get("lat"),
+            lon=vehicle_element.get("lon"),
+            secs_since_report=vehicle_element.get("secs_since_report"),
+            predictable=vehicle_element.get("predictable"),
+            heading=vehicle_element.get("heading"),
+            speed_km_hr=vehicle_element.get("speed_km_hr"),
+            last_time=last_time
+        )
+        vehicle_location_snapshots.append(vehicle_location_snapshot_obj)
+    print(len(vehicle_location_snapshots))
+    pass
 
 
 main()
+
--- vehiclelocations_module.py
+++ vehiclelocations_module.py
@@ -1,36 +1,56 @@
 vehicleLocations = """<?xml version="1.0" encoding="utf-8" ?>
 <body copyright="All data copyright Toronto Transit Commission 2023.">
-    <vehicle id="4575" routeTag="501" dirTag="501_0_501" lat="43.6682668" lon="-79.3079405" secsSinceReport="27" predictable="true" heading="72" speedKmHr="29"/>
-    <vehicle id="4531" routeTag="501" dirTag="501_0_501" lat="43.6451889" lon="-79.4135542" secsSinceReport="27" predictable="true" heading="74" speedKmHr="0"/>
-    <vehicle id="8379" lat="43.6483583" lon="-79.4041733" secsSinceReport="86" predictable="false" heading="-4" speedKmHr="0"/>
-    <vehicle id="3316" routeTag="501" dirTag="501_0_501Lbus" lat="43.6360805" lon="-79.4689157" secsSinceReport="26" predictable="true" heading="323" speedKmHr="38"/>
-    <vehicle id="4565" routeTag="501" dirTag="501_1_501" lat="43.6549003" lon="-79.387973" secsSinceReport="27" predictable="true" heading="264" speedKmHr="9"/>
-    <vehicle id="4600" routeTag="501" dirTag="501_0_501" lat="43.654438" lon="-79.3904851" secsSinceReport="27" predictable="true" heading="96" speedKmHr="12"/>
-    <vehicle id="8101" routeTag="501" dirTag="501_0_501Lbus" lat="43.644454" lon="-79.4170341" secsSinceReport="26" predictable="true" heading="74" speedKmHr="0"/>
-    <vehicle id="8346" routeTag="501" lat="43.6429334" lon="-79.415675" secsSinceReport="26" predictable="false" heading="-4" speedKmHr="0"/>
-    <vehicle id="4586" routeTag="501" dirTag="501_1_501" lat="43.6680759" lon="-79.3090156" secsSinceReport="27" predictable="true" heading="252" speedKmHr="21"/>
-    <vehicle id="8340" routeTag="501" dirTag="501_0_501Bbus" lat="43.6474665" lon="-79.4040216" secsSinceReport="27" predictable="true" heading="216" speedKmHr="0"/>
-    <vehicle id="1345" routeTag="501" dirTag="501_0_501Lbus" lat="43.60136" lon="-79.5025737" secsSinceReport="26" predictable="true" heading="72" speedKmHr="47"/>
-    <vehicle id="1026" routeTag="501" lat="43.6664483" lon="-79.438115" secsSinceReport="26" predictable="false" heading="342" speedKmHr="29"/>
-    <vehicle id="4591" routeTag="501" dirTag="501_0_501" lat="43.6603431" lon="-79.3430862" secsSinceReport="27" predictable="true" heading="73" speedKmHr="0"/>
-    <vehicle id="8315" routeTag="501" dirTag="501_0_501Bbus" lat="43.6478984" lon="-79.4041296" secsSinceReport="26" predictable="true" heading="216" speedKmHr="0"/>
-    <vehicle id="4596" routeTag="501" dirTag="501_1_501" lat="43.662276" lon="-79.3512622" secsSinceReport="27" predictable="true" heading="343" speedKmHr="0"/>
-    <vehicle id="4597" routeTag="501" dirTag="501_1_501" lat="43.6571617" lon="-79.3743743" secsSinceReport="27" predictable="true" heading="256" speedKmHr="0"/>
-    <vehicle id="4476" routeTag="501" lat="43.6413916" lon="-79.4151883" secsSinceReport="27" predictable="false" heading="-4" speedKmHr="0"/>
-    <vehicle id="8194" routeTag="501" lat="43.6483" lon="-79.4040166" secsSinceReport="26" predictable="false" heading="-4" speedKmHr="0"/>
-    <vehicle id="4587" routeTag="501" dirTag="501_1_501" lat="43.6562861" lon="-79.3784418" secsSinceReport="27" predictable="true" heading="253" speedKmHr="17"/>
-    <vehicle id="4424" routeTag="501" dirTag="501_1_501" lat="43.6387669" lon="-79.4462343" secsSinceReport="27" predictable="true" heading="256" speedKmHr="6"/>
-    <vehicle id="8305" routeTag="501" lat="43.6656716" lon="-79.411415" secsSinceReport="26" predictable="false" heading="343" speedKmHr="39"/>
-    <vehicle id="4547" routeTag="501" dirTag="501_1_NERQ" lat="43.6421432" lon="-79.4293365" secsSinceReport="27" predictable="true" heading="216" speedKmHr="0"/>
-    <vehicle id="4549" routeTag="501" dirTag="501_0_501" lat="43.6668395" lon="-79.3141021" secsSinceReport="27" predictable="true" heading="72" speedKmHr="20"/>
-    <vehicle id="4429" routeTag="501" dirTag="501_0_501" lat="43.6602969" lon="-79.3617548" secsSinceReport="27" predictable="true" heading="72" speedKmHr="18"/>
-    <vehicle id="4483" routeTag="501" dirTag="501_0_501" lat="43.6712585" lon="-79.2942204" secsSinceReport="27" predictable="true" heading="74" speedKmHr="24"/>
-    <vehicle id="4487" routeTag="501" dirTag="501_1_501" lat="43.6385733" lon="-79.4471216" secsSinceReport="27" predictable="true" heading="317" speedKmHr="0"/>
-    <vehicle id="3354" routeTag="501" dirTag="501_1_501Lbus" lat="43.6019124" lon="-79.5003479" secsSinceReport="27" predictable="true" heading="256" speedKmHr="1"/>
-    <vehicle id="8085" routeTag="501" lat="43.6163966" lon="-79.5282883" secsSinceReport="26" predictable="false" heading="-4" speedKmHr="0"/>
-    <vehicle id="4518" routeTag="501" dirTag="501_0_501" lat="43.6445617" lon="-79.4165115" secsSinceReport="27" predictable="true" heading="74" speedKmHr="1"/>
-    <vehicle id="3547" routeTag="501" dirTag="501_0_501Lbus" lat="43.6173024" lon="-79.4872622" secsSinceReport="27" predictable="true" heading="22" speedKmHr="35"/>
-    <vehicle id="1402" routeTag="501" lat="43.5929024" lon="-79.5406079" secsSinceReport="27" predictable="false" heading="-4" speedKmHr="0"/>
-    <vehicle id="4513" routeTag="501" lat="43.6647377" lon="-79.3237304" secsSinceReport="27" predictable="false" heading="-4" speedKmHr="0"/>
-    <lastTime time="1690942782273"/>
+    <vehicle id="8377" routeTag="501" lat="43.64819" lon="-79.404015" secsSinceReport="13" predictable="false" heading="-4" speedKmHr="0"/>
+    <vehicle id="8216" routeTag="501" dirTag="501_1_501Lbus" lat="43.6117288" lon="-79.4899911" secsSinceReport="13" predictable="true" heading="186" speedKmHr="5"/>
+    <vehicle id="8139" routeTag="501" lat="43.5929116" lon="-79.5405711" secsSinceReport="13" predictable="false" heading="-4" speedKmHr="0"/>
+    <vehicle id="4530" routeTag="501" dirTag="501_0_501" lat="43.6708488" lon="-79.2959289" secsSinceReport="14" predictable="true" heading="73" speedKmHr="0"/>
+    <vehicle id="8136" routeTag="501" dirTag="501_1_501Bbus" lat="43.6520608" lon="-79.3774082" secsSinceReport="13" predictable="true" heading="253" speedKmHr="0"/>
+    <vehicle id="8098" routeTag="501" dirTag="501_0_501Lbus" lat="43.6402793" lon="-79.4381463" secsSinceReport="13" predictable="true" heading="80" speedKmHr="12"/>
+    <vehicle id="3316" routeTag="501" dirTag="501_0_501Lbus" lat="43.6005488" lon="-79.5060022" secsSinceReport="13" predictable="true" heading="72" speedKmHr="33"/>
+    <vehicle id="3558" routeTag="501" dirTag="501_1_501Bbus" lat="43.653245" lon="-79.3755766" secsSinceReport="13" predictable="true" heading="252" speedKmHr="0"/>
+    <vehicle id="4565" routeTag="501" dirTag="501_1_501" lat="43.6546909" lon="-79.389485" secsSinceReport="14" predictable="true" heading="254" speedKmHr="1"/>
+    <vehicle id="4566" routeTag="501" dirTag="501_1_501" lat="43.6613538" lon="-79.3577146" secsSinceReport="13" predictable="true" heading="255" speedKmHr="0"/>
+    <vehicle id="4524" routeTag="501" dirTag="501_0_501" lat="43.6569046" lon="-79.3749158" secsSinceReport="14" predictable="true" heading="77" speedKmHr="0"/>
+    <vehicle id="4525" routeTag="501" dirTag="501_1_501" lat="43.6564021" lon="-79.3797558" secsSinceReport="13" predictable="true" heading="287" speedKmHr="5"/>
+    <vehicle id="4449" routeTag="501" dirTag="501_0_501" lat="43.6593666" lon="-79.3659286" secsSinceReport="14" predictable="true" heading="73" speedKmHr="0"/>
+    <vehicle id="3518" routeTag="501" dirTag="501_1_501Bbus" lat="43.6492566" lon="-79.3941354" secsSinceReport="13" predictable="true" heading="253" speedKmHr="16"/>
+    <vehicle id="8341" routeTag="501" dirTag="501_1_501Bbus" lat="43.6490429" lon="-79.3952479" secsSinceReport="13" predictable="true" heading="253" speedKmHr="21"/>
+    <vehicle id="7970" routeTag="501" dirTag="501_0_501Lbus" lat="43.626823" lon="-79.4792244" secsSinceReport="13" predictable="true" heading="21" speedKmHr="15"/>
+    <vehicle id="8144" routeTag="501" lat="43.6483366" lon="-79.4040799" secsSinceReport="13" predictable="false" heading="322" speedKmHr="2"/>
+    <vehicle id="7972" routeTag="501" dirTag="501_0_501Lbus" lat="43.6410636" lon="-79.4171829" secsSinceReport="13" predictable="true" heading="256" speedKmHr="0"/>
+    <vehicle id="4420" routeTag="501" dirTag="501_0_501" lat="43.6388511" lon="-79.4454116" secsSinceReport="14" predictable="true" heading="77" speedKmHr="0"/>
+    <vehicle id="8345" routeTag="501" dirTag="501_0_501Bbus" lat="43.66321" lon="-79.3516716" secsSinceReport="13" predictable="true" heading="344" speedKmHr="31"/>
+    <vehicle id="4586" routeTag="501" dirTag="501_0_501" lat="43.662561" lon="-79.3332122" secsSinceReport="13" predictable="true" heading="76" speedKmHr="0"/>
+    <vehicle id="8346" routeTag="501" dirTag="501_1_501Bbus" lat="43.6551628" lon="-79.3668136" secsSinceReport="13" predictable="true" heading="255" speedKmHr="6"/>
+    <vehicle id="8181" routeTag="501" lat="43.6650446" lon="-79.3543471" secsSinceReport="14" predictable="false" heading="-4" speedKmHr="0"/>
+    <vehicle id="8384" routeTag="501" dirTag="501_0_501Bbus" lat="43.6579916" lon="-79.3537174" secsSinceReport="13" predictable="true" heading="73" speedKmHr="13"/>
+    <vehicle id="1071" routeTag="501" dirTag="501_1_501Lbus" lat="43.6332893" lon="-79.4739151" secsSinceReport="13" predictable="true" heading="232" speedKmHr="52"/>
+    <vehicle id="1423" routeTag="501" dirTag="501_1_501Lbus" lat="43.6112632" lon="-79.4901351" secsSinceReport="13" predictable="true" heading="183" speedKmHr="0"/>
+    <vehicle id="1420" routeTag="501" dirTag="501_1_501Lbus" lat="43.6389669" lon="-79.4451162" secsSinceReport="14" predictable="true" heading="255" speedKmHr="0"/>
+    <vehicle id="3564" routeTag="501" dirTag="501_0_501Bbus" lat="43.6507902" lon="-79.3811899" secsSinceReport="13" predictable="true" heading="163" speedKmHr="0"/>
+    <vehicle id="4415" routeTag="501" dirTag="501_1_501" lat="43.6563933" lon="-79.3809168" secsSinceReport="14" predictable="true" heading="255" speedKmHr="17"/>
+    <vehicle id="4592" routeTag="501" dirTag="501_1_501" lat="43.6408862" lon="-79.4354765" secsSinceReport="13" predictable="true" heading="255" speedKmHr="0"/>
+    <vehicle id="4551" routeTag="501" dirTag="501_0_501" lat="43.6671866" lon="-79.3128666" secsSinceReport="13" predictable="true" heading="252" speedKmHr="15"/>
+    <vehicle id="4596" routeTag="501" dirTag="501_1_501" lat="43.6404916" lon="-79.437427" secsSinceReport="14" predictable="true" heading="254" speedKmHr="21"/>
+    <vehicle id="4476" routeTag="501" dirTag="501_1_501" lat="43.673705" lon="-79.281335" secsSinceReport="14" predictable="true" heading="216" speedKmHr="0"/>
+    <vehicle id="8192" routeTag="501" dirTag="501_1_501Bbus" lat="43.6635818" lon="-79.3592681" secsSinceReport="12" predictable="true" heading="167" speedKmHr="9"/>
+    <vehicle id="1085" routeTag="501" dirTag="501_1_501Lbus" lat="43.6405258" lon="-79.4278717" secsSinceReport="13" predictable="true" heading="344" speedKmHr="46"/>
+    <vehicle id="8153" routeTag="501" dirTag="501_0_501Lbus" lat="43.6257921" lon="-79.4797007" secsSinceReport="13" predictable="true" heading="20" speedKmHr="25"/>
+    <vehicle id="8195" routeTag="501" lat="43.6482416" lon="-79.4043916" secsSinceReport="13" predictable="false" heading="201" speedKmHr="11"/>
+    <vehicle id="4467" routeTag="501" dirTag="501_0_501" lat="43.6420117" lon="-79.4293038" secsSinceReport="13" predictable="true" heading="74" speedKmHr="3"/>
+    <vehicle id="4425" routeTag="501" dirTag="501_0_501" lat="43.645765" lon="-79.4111516" secsSinceReport="14" predictable="true" heading="73" speedKmHr="16"/>
+    <vehicle id="4549" routeTag="501" dirTag="501_1_501" lat="43.6648795" lon="-79.323086" secsSinceReport="14" predictable="true" heading="252" speedKmHr="15"/>
+    <vehicle id="8365" routeTag="501" dirTag="501_1_501Lbus" lat="43.592715" lon="-79.5417666" secsSinceReport="14" predictable="true" heading="251" speedKmHr="0"/>
+    <vehicle id="8363" routeTag="501" dirTag="501_1_501Bbus" lat="43.6552581" lon="-79.3663406" secsSinceReport="13" predictable="true" heading="251" speedKmHr="15"/>
+    <vehicle id="4483" routeTag="501" dirTag="501_1_501" lat="43.6503486" lon="-79.3898315" secsSinceReport="14" predictable="true" heading="165" speedKmHr="1"/>
+    <vehicle id="8166" routeTag="501" dirTag="501_0_501Lbus" lat="43.6445792" lon="-79.4163" secsSinceReport="12" predictable="true" heading="112" speedKmHr="10"/>
+    <vehicle id="4441" routeTag="501" dirTag="501_0_501" lat="43.6614389" lon="-79.3380527" secsSinceReport="14" predictable="true" heading="73" speedKmHr="13"/>
+    <vehicle id="8323" routeTag="501" dirTag="501_0_501Lbus" lat="43.5951538" lon="-79.5304107" secsSinceReport="12" predictable="true" heading="72" speedKmHr="0"/>
+    <vehicle id="4487" routeTag="501" dirTag="501_1_501" lat="43.6386264" lon="-79.4472151" secsSinceReport="13" predictable="true" heading="274" speedKmHr="10"/>
+    <vehicle id="4520" routeTag="501" dirTag="501_0_501" lat="43.6610984" lon="-79.3582687" secsSinceReport="13" predictable="true" heading="70" speedKmHr="0"/>
+    <vehicle id="3347" routeTag="501" dirTag="501_0_501Lbus" lat="43.5925069" lon="-79.541966" secsSinceReport="14" predictable="true" heading="73" speedKmHr="32"/>
+    <vehicle id="1089" routeTag="501" dirTag="501_1_501Lbus" lat="43.6023826" lon="-79.4983596" secsSinceReport="13" predictable="true" heading="218" speedKmHr="1"/>
+    <vehicle id="4557" routeTag="501" dirTag="501_1_501" lat="43.652154" lon="-79.3905759" secsSinceReport="14" predictable="true" heading="165" speedKmHr="22"/>
+    <vehicle id="4513" routeTag="501" dirTag="501_0_501" lat="43.6561899" lon="-79.3811839" secsSinceReport="14" predictable="true" heading="73" speedKmHr="10"/>
+    <lastTime time="1691014564579"/>
 </body>"""
